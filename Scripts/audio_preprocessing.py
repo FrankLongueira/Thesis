Fixed outsample scaling

Scripts/audio_preprocessing.py
@@ -80,9 +80,9 @@ def generate_train_features( x_train ):
 		x_train_features[:, i] = np.abs(np.fft.fft( a = x_train[:, i], n = n ))
 	return(x_train_features)
 	
-def scale_features( x_train_features, mu, sd ):
+def scale_features( x_train_features, mu, std ):
 	
-	x_train_features = (x_train_features - mu) / float(sd)
+	x_train_features = (x_train_features - mu) / float(std)
 		
 	return(x_train_features.T)
 	

Scripts/main.py
@@ -26,17 +26,17 @@ print("Creating training & test sets...")
 training_chapter_names = ["Chapter1"]
 audio_time_series_train, fs = ap.concatenate_audio( training_chapter_names, chapters )
 train_mu = np.mean( audio_time_series_train )
-train_sd = np.sd( audio_time_series_train )
+train_std = np.std( audio_time_series_train )
 
 x_train = ap.generate_frames( audio_time_series_train, fs, frame_time = 0.020 )
-x_train_scaled = ap.scale_features( x_train, train_mu, train_sd )
+x_train_scaled = ap.scale_features( x_train, train_mu, train_std )
 x_train_scaled_input = np.reshape(x_train_scaled, (x_train_scaled.shape[0], x_train_scaled.shape[1], 1))
 
 test_chapter_names = ["Chapter2"]
 audio_time_series_test, fs = ap.concatenate_audio( test_chapter_names, chapters )
 audio_time_series_test = audio_time_series_test[0:60*fs]
 x_test = ap.generate_frames( audio_time_series_test, fs, frame_time = 0.020 )
-x_test_scaled = ap.scale_features( x_test, train_mu, train_sd )
+x_test_scaled = ap.scale_features( x_test, train_mu, train_std )
 x_test_scaled_input = np.reshape(x_test_scaled, (x_test_scaled.shape[0], x_test_scaled.shape[1], 1))
 
 # Build Neural Network
@@ -61,7 +61,7 @@ dcam.save_model(model, model_save_path)
 # Then match test set utterances with closest utterances in training utterance embedding
 print("Encoding & flattening training/test sets...")
 #x_train_encoded_flattened = clus.encode_and_flatten(model, x_train_scaled_input)
-x_test_encoded_flattened = train_scale_factor * clus.encode_and_flatten(model, x_test_scaled_input)
+x_test_encoded_flattened = (train_std * clus.encode_and_flatten(model, x_test_scaled_input)) + train_mu
 print(x_test_encoded_flattened)
 print(x_test_encoded_flattened.shape)
 