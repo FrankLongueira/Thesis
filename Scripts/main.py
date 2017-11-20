b8a537ec8e7524ed2b35d26753212291a0b99645
Frank Longueira <franklongueira@gmail.com>
2017-11-20 01:03:40 -0500

    Testing white noise

--- Scripts/main.py
+++ Scripts/main.py
@@ -27,21 +27,30 @@ training_chapter_names = ["Chapter1"]
 audio_time_series_train, fs = ap.concatenate_audio( training_chapter_names, chapters )
 train_mu = np.mean( audio_time_series_train )
 train_std = np.std( audio_time_series_train )
+print(np.min(audio_time_series_train), np.max(audio_time_series_train), np.mean(audio_time_series_train), np.std(audio_time_series_train))
+
+audio_time_series_train_noisy = audio_time_series_train + np.random.normal(loc=0.0, scale= np.std(audio_time_series_train)/4, size = audio_time_series_train.shape)
+
 
 x_train = ap.generate_frames( audio_time_series_train, fs, frame_time = 0.020 )
 x_train_scaled = ap.scale_features( x_train, train_mu, train_std )
 x_train_scaled_input = np.reshape(x_train_scaled, (x_train_scaled.shape[0], x_train_scaled.shape[1], 1))
 
+x_train_noisy = ap.generate_frames( audio_time_series_train_noisy, fs, frame_time = 0.020 )
+x_train_noisy_scaled = ap.scale_features( x_train_noisy, train_mu, train_std )
+x_train_noisy_scaled_input = np.reshape(x_train_noisy_scaled, (x_train_scaled.shape[0], x_train_scaled.shape[1], 1))
+
 test_chapter_names = ["Chapter2"]
 audio_time_series_test, fs = ap.concatenate_audio( test_chapter_names, chapters )
 audio_time_series_test = audio_time_series_test[0:60*fs]
-x_test = ap.generate_frames( audio_time_series_test, fs, frame_time = 0.020 )
-x_test_scaled = ap.scale_features( x_test, train_mu, train_std )
-x_test_scaled_input = np.reshape(x_test_scaled, (x_test_scaled.shape[0], x_test_scaled.shape[1], 1))
+audio_time_series_test_noisy = audio_time_series_test + np.random.normal(loc=0.0, scale= np.std(audio_time_series_train)/4, size = audio_time_series_test.shape)
+x_test_noisy = ap.generate_frames( audio_time_series_test_noisy, fs, frame_time = 0.020 )
+x_test_noisy_scaled = ap.scale_features( x_test, train_mu, train_std )
+x_test_noisy_scaled_input = np.reshape(x_test_scaled, (x_test_scaled.shape[0], x_test_scaled.shape[1], 1))
 
 # Build Neural Network
 print("Preparing neural network for training...")
-input_shape = (x_train.shape[0], 1)
+input_shape = (x_train_noisy.shape[0], 1)
 filter_size = int(0.005*fs)
 
 model = dcam.create_model( input_shape, filter_size,)
@@ -49,7 +58,7 @@ model = dcam.create_model( input_shape, filter_size,)
 # Train Neural Network
 epochs = 5
 batch_size = 100
-model = dcam.train_model( model = model, inputs = x_train_scaled_input, labels = x_train_scaled_input, epochs = epochs, batch_size = batch_size )
+model = dcam.train_model( model = model, inputs = x_train_noisy_scaled_input, labels = x_train_scaled_input, epochs = epochs, batch_size = batch_size )
 
 # Save/load model
 model_save_path = parent_cwd + "/Saved_Models/Current_CNN_Model"
@@ -61,7 +70,7 @@ dcam.save_model(model, model_save_path)
 # Then match test set utterances with closest utterances in training utterance embedding
 print("Encoding & flattening training/test sets...")
 #x_train_encoded_flattened = clus.encode_and_flatten(model, x_train_scaled_input)
-x_test_encoded_flattened = (train_std * clus.encode_and_flatten(model, x_test_scaled_input)) + train_mu
+x_test_encoded_flattened = (train_std * clus.encode_and_flatten(model, x_test_noisy_scaled_input)) + train_mu
 print(x_test_encoded_flattened)
 print(x_test_encoded_flattened.shape)
 
