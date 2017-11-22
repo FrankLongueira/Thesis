eb4a07328a9ce82c7c654dd4dc62fe738682317a
Frank Longueira <franklongueira@gmail.com>
2017-11-22 00:16:29 -0500

    SNR Test 2

--- Scripts/audio_preprocessing.py
+++ Scripts/audio_preprocessing.py
@@ -67,6 +67,8 @@ def combine_clean_and_noise(audio_time_series_train, audio_time_series_noise, sn
 	A_noise_targ = np.sqrt( (A_train**2) / (10**(snr_db/10.)) )
 	
 	scaling_coeff = A_noise_targ / A_noise
+	
+	print(A_train, A_noise, A_noise_targ, scaling_coeff)
 	audio_time_series_noise = scaling_coeff * audio_time_series_noise
 	
 	combined_result = audio_time_series_train + audio_time_series_noise
