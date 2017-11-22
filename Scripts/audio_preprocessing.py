dafeb49e3ec78b83c4c051c89c94e75185f8ba56
Frank Longueira <franklongueira@gmail.com>
2017-11-22 00:29:44 -0500

    Fixed typo in combine

--- Scripts/audio_preprocessing.py
+++ Scripts/audio_preprocessing.py
@@ -71,7 +71,7 @@ def combine_clean_and_noise(audio_time_series_train, audio_time_series_noise, sn
 	
 	scaling_coeff = np.sqrt(A_noise_targ_2) / np.sqrt(A_noise_2)
 	
-	print(A_train, A_noise, A_noise_targ, scaling_coeff)
+	print(A_train_2, A_noise_2, A_noise_targ_2, scaling_coeff)
 	audio_time_series_noise = scaling_coeff * audio_time_series_noise
 	
 	combined_result = audio_time_series_train + audio_time_series_noise
