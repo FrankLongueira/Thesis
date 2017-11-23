53e2d4518950016acfbfd6e9b14ffb88c571c98d
Frank Longueira <franklongueira@gmail.com>
2017-11-22 20:35:44 -0500

    Gaussian noise 10 dB test

--- Scripts/main.py
+++ Scripts/main.py
@@ -30,6 +30,7 @@ train_mu = np.mean( audio_time_series_train )
 train_std = np.std( audio_time_series_train )
 
 audio_time_series_train_noise, fs = ap.concatenate_audio( training_noise_names, noise )
+audio_time_series_train_noise = np.random.normal(0, 1, audio_time_series_train.size) 
 
 snr_db = 10
 audio_time_series_train_noisy = ap.combine_clean_and_noise(audio_time_series_train, audio_time_series_train_noise, snr_db)
@@ -48,6 +49,8 @@ audio_time_series_test, fs = ap.concatenate_audio( test_chapter_names, chapters 
 audio_time_series_noise, fs = ap.concatenate_audio( test_noise_names, noise )
 audio_time_series_test = audio_time_series_test[0:60*fs]
 audio_time_series_test_noise = audio_time_series_noise[0:60*fs]
+audio_time_series_test_noise = np.random.normal(0, 1, audio_time_series_test.size) 
+
 audio_time_series_test_noisy = ap.combine_clean_and_noise(audio_time_series_test, audio_time_series_test_noise, snr_db)
 
 x_test_noisy = ap.generate_frames( audio_time_series_test_noisy, fs, frame_time = 0.020 )
@@ -62,7 +65,7 @@ filter_size = int(0.005*fs)
 model = dcam.create_model( input_shape, filter_size )
 
 # Train Neural Network
-epochs = 100
+epochs = 5
 batch_size = 100
 model = dcam.train_model( model = model, inputs = x_train_noisy_scaled_input, labels = x_train_scaled_input, epochs = epochs, batch_size = batch_size )
 
