2e5f04f8ec11520582d6260eecb5b19be9fb502b
Frank Longueira <franklongueira@gmail.com>
2017-11-22 23:55:02 -0500

    256 filters

--- Scripts/dca_model.py
+++ Scripts/dca_model.py
@@ -19,7 +19,7 @@ def create_model(input_shape, filter_size):
 	#model.add(MaxPooling1D(pool_size = 4, padding='same'))
 	
 	#model.add(BatchNormalization())
-	model.add(Conv1D(filters = 128, kernel_size = filter_size, padding='same', input_shape = input_shape))
+	model.add(Conv1D(filters = 256, kernel_size = filter_size, padding='same', input_shape = input_shape))
 	model.add(BatchNormalization())
 	model.add(Activation('relu'))
 	#model.add(MaxPooling1D(pool_size = 4, padding='same'))
