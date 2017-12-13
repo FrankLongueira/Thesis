a0e15f61314e98630d70f565689206e98feb2bab
Frank Longueira <franklongueira@gmail.com>
2017-12-12 23:49:24 -0500

    Training Model3 100 epochs

--- Scripts/main.py
+++ Scripts/main.py
@@ -70,7 +70,7 @@ print("Preparing neural network for training...")
 input_shape = (x_train_noisy.shape[0], 1)
 filter_size = int(0.005*fs)
 model = dcam.create_model( input_shape, filter_size )
-epochs = 50
+epochs = 100
 batch_size = 100
 
 model_save_path = parent_cwd + "/Saved_Models/Model3"
