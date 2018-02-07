69d019da7f6e90cacb91ea5553969428f277f005
Frank Longueira <franklongueira@gmail.com>
2018-02-06 22:36:35 -0500

    computing pesq for validation set

--- Scripts/audio_preprocessing.py
+++ Scripts/audio_preprocessing.py
@@ -148,4 +148,4 @@ def generate_input( audio_time_series, fs, frame_time, train_mu, train_std ):
 	x_frames_scaled = scale_features( x_frames, train_mu, train_std )
 	x_frames_scaled_input = np.reshape(x_frames_scaled, (x_frames_scaled.shape[0], x_frames_scaled.shape[1], 1))
 	
-	return(x_frames_input)
+	return(x_frames_scaled_input)
