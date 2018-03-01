e98dbc4deba417bfa5ab954a397e3360f1e612ac
Frank Longueira <franklongueira@gmail.com>
2018-02-28 22:17:46 -0500

    triciag testing

--- Scripts/main.py
+++ Scripts/main.py
@@ -20,10 +20,10 @@ audio_time_series_train_noise, fs = ap.load_audio( audio_folder_path, audio_file
 audio_time_series_train_noisy = ap.combine_clean_and_noise(audio_time_series_train, audio_time_series_train_noise, snr_db)
 train_mu = np.mean( audio_time_series_train )
 train_std = np.std( audio_time_series_train )
-
+"""
 train_clean = ap.generate_input(  audio_time_series_train, fs, frame_time, train_mu, train_std )
 train_noisy = ap.generate_input(  audio_time_series_train_noisy, fs, frame_time, train_mu, train_std )
-"""
+
 # Generate validation set
 audio_time_series_validation, fs = ap.load_audio( audio_folder_path, audio_filename = "Chapter2_5_Min.wav")
 audio_time_series_validation_noise, fs = ap.load_audio( audio_folder_path, audio_filename = "Chapter2_5_Min_Babble.wav")
@@ -33,7 +33,7 @@ validation_clean = ap.generate_input(  audio_time_series_validation, fs, frame_t
 validation_noisy = ap.generate_input(  audio_time_series_validation_noisy, fs, frame_time, train_mu, train_std )
 """
 # Generate test set
-audio_time_series_test, fs = ap.load_audio( audio_folder_path, audio_filename = "Chapter3_5_Min.wav")
+audio_time_series_test, fs = ap.load_audio( audio_folder_path, audio_filename = "TriciaG_5Min.wav")
 audio_time_series_test_noise, fs = ap.load_audio( audio_folder_path, audio_filename = "Chapter3_5_Min_Babble.wav")
 audio_time_series_test_noisy = ap.combine_clean_and_noise(audio_time_series_test, audio_time_series_test_noise, snr_db)
 
@@ -58,11 +58,11 @@ test_noisy = ap.generate_input(  audio_time_series_test_noisy, fs, frame_time, t
 #										validation_labels = validation_clean,
 #										filepath = model_save_path)
 #	cnn.save_model(model, model_save_path)
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "CleanTest_5min.wav", rate = fs, data = audio_time_series_test.astype('int16'))
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "NoisyTest_5dB_5min.wav", rate = fs, data = audio_time_series_test_noisy.astype('int16'))
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "TriciaG_5min.wav", rate = fs, data = audio_time_series_test.astype('int16'))
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "TriciaGNoisy_5dB_5min.wav", rate = fs, data = audio_time_series_test_noisy.astype('int16'))
 
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "CleanTest_1min.wav", rate = fs, data = audio_time_series_test.astype('int16')[0:(60*fs)])
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "NoisyTest_5dB_1min.wav", rate = fs, data = audio_time_series_test_noisy.astype('int16')[0:(60*fs)])
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "TriciaG_1min.wav", rate = fs, data = audio_time_series_test.astype('int16')[0:(60*fs)])
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "TriciaGNoisy_5dB_1min.wav", rate = fs, data = audio_time_series_test_noisy.astype('int16')[0:(60*fs)])
 
 model_name = "Model_65"
 model_save_path = parent_cwd + "/Saved_Models/" + model_name
@@ -72,10 +72,11 @@ print("Getting CNN output for noisy test set input...")
 test_filtered_frames = (train_std * cnn.get_output_multiple_batches(model, test_noisy)) + train_mu
 print("Perfectly reconstructing filtered test set audio & saving to memory...")
 test_filtered = ap.rebuild_audio( test_filtered_frames )
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_FilteredTest_5min.wav", rate = fs, data = test_filtered)
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_FilteredTest_1min.wav", rate = fs, data = test_filtered[0:(60*fs)])
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_TriciaGFiltered_5min.wav", rate = fs, data = test_filtered)
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_TriciaGFiltered_1min.wav", rate = fs, data = test_filtered[0:(60*fs)])
 os.chdir(parent_cwd + "/Audio_Files/Test_Files")
-call( ["./PESQ", "+16000", "CleanTest_1min.wav", model_name + "_FilteredTest_1min.wav"] )
+call( ["./PESQ", "+16000", "TriciaG_1min.wav", model_name + "_TriciaGFiltered_1min.wav"] )
+call( ["./PESQ", "+16000", "TriciaG_1min.wav", "TriciaGNoisy_5dB_1min.wav"] )
 
 #	summary_stats_filename = parent_cwd + "/Saved_Models/Model_Descriptions.txt"
 #	cnn.summary_statistics( summary_stats_filename, model_name, history, frame_time, snr_db, 
