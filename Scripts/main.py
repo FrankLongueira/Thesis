2dc200004ccb7600979d4a17d9f108b515f61417
Frank Longueira <franklongueira@gmail.com>
2018-03-09 19:49:58 -0500

    best model 5dB test

--- Scripts/main.py
+++ Scripts/main.py
@@ -11,7 +11,7 @@ parent_cwd = os.path.abspath(os.path.join(cwd, os.pardir))
 audio_folder_path = parent_cwd + "/Audio_Files/"
 
 print("Creating training, validation, and test sets...")
-snr_db = -5
+snr_db = 5
 frame_time = 0.020
 
 # Generate training set
@@ -24,6 +24,7 @@ train_std = np.std( audio_time_series_train )
 train_clean = ap.generate_input(  audio_time_series_train, fs, frame_time, train_mu, train_std )
 train_noisy = ap.generate_input(  audio_time_series_train_noisy, fs, frame_time, train_mu, train_std )
 
+"""
 # Generate validation set
 audio_time_series_validation, fs = ap.load_audio( audio_folder_path, audio_filename = "Chapter2_5_Min.wav")
 scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "CleanValidation_5min_" + str(snr_db) + "dB.wav", rate = fs, data = audio_time_series_validation.astype('int16'))
@@ -37,7 +38,7 @@ scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "No
 
 validation_clean = ap.generate_input(  audio_time_series_validation, fs, frame_time, train_mu, train_std )
 validation_noisy = ap.generate_input(  audio_time_series_validation_noisy, fs, frame_time, train_mu, train_std )
-
+"""
 # Generate test set
 audio_time_series_test, fs = ap.load_audio( audio_folder_path, audio_filename = "Chapter3_5_Min.wav")
 scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "CleanTest_5min_" + str(snr_db) + "dB.wav", rate = fs, data = audio_time_series_test.astype('int16'))
@@ -50,7 +51,7 @@ scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + "No
 
 test_clean = ap.generate_input(  audio_time_series_test, fs, frame_time, train_mu, train_std )
 test_noisy = ap.generate_input(  audio_time_series_test_noisy, fs, frame_time, train_mu, train_std )
-
+"""
 print("Preparing neural network for training...")
 input_shape = (train_noisy.shape[1], 1)
 epochs = 300
@@ -59,11 +60,12 @@ filter_size_per_hidden_layer = [0.005, 0.005, 0.005, 0.005, 0.005]
 filter_size_output_layer = 0.005
 num_filters_per_hidden_layer = [12, 25, 50, 100, 200]
 patience = 20
-
-model_name = "Model_53_PReLU" + str(snr_db)
+"""
+#model_name = "Model_53_PReLU" + str(snr_db)
+model_name = "Model53_PReLU"
 model_save_path = parent_cwd + "/Saved_Models/" + model_name
-#model = cnn.load_model_(model_save_path)
-
+model = cnn.load_model_(model_save_path)
+"""
 model = cnn.create_model( input_shape, num_filters_per_hidden_layer, map(int, list(np.array(filter_size_per_hidden_layer)*fs)), int(filter_size_output_layer*fs) )
 model, history = cnn.train_model( 	model = model, 
 									train_inputs = train_noisy, 
@@ -77,18 +79,18 @@ model, history = cnn.train_model( 	model = model,
 									
 cnn.save_model(model, model_save_path)
 #model = cnn.load_model_(model_save_path)
-
+"""
 print("Getting CNN output for noisy test set input...")
-test_filtered_frames = (train_std * cnn.get_output_multiple_batches(model, validation_noisy)) + train_mu
+test_filtered_frames = (train_std * cnn.get_output_multiple_batches(model, test_noisy)) + train_mu
 
 print("Perfectly reconstructing filtered test set audio & saving to memory...")
 test_filtered = ap.rebuild_audio( test_filtered_frames )
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_FilteredValidation_5min_" + str(snr_db) + "dB.wav", rate = fs, data = test_filtered)
-scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_FilteredValidation_1min_" + str(snr_db) + "dB.wav", rate = fs, data = test_filtered[0:(60*fs)])
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_FilteredTest_5min_" + str(snr_db) + "dB.wav", rate = fs, data = test_filtered)
+scipy.io.wavfile.write( filename = parent_cwd + "/Audio_Files/Test_Files/" + model_name + "_FilteredTest_1min_" + str(snr_db) + "dB.wav", rate = fs, data = test_filtered[0:(60*fs)])
 
 os.chdir(parent_cwd + "/Audio_Files/Test_Files")
-call( ["./PESQ", "+16000", "CleanValidation_1min_" + str(snr_db) + "dB.wav", "NoisyValidation_1min_" + str(snr_db) + "dB.wav"] )
-call( ["./PESQ", "+16000", "CleanValidation_1min_" + str(snr_db) + "dB.wav", model_name + "_FilteredValidation_1min_" + str(snr_db) + "dB.wav"] )
+call( ["./PESQ", "+16000", "CleanTest_1min_" + str(snr_db) + "dB.wav", "NoisyTest_1min_" + str(snr_db) + "dB.wav"] )
+call( ["./PESQ", "+16000", "CleanTest_1min_" + str(snr_db) + "dB.wav", model_name + "_FilteredTest_1min_" + str(snr_db) + "dB.wav"] )
 
 summary_stats_filename = parent_cwd + "/Saved_Models/Model_Descriptions.txt"
 cnn.summary_statistics( summary_stats_filename, model_name, history, frame_time, snr_db, 
